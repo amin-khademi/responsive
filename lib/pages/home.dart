c6169f09f203d2fc375ba00df9f0ccb6e6a053fb
amin.khademi <120728947+amin-khademi@users.noreply.github.com>
2024-03-21 13:12:47 +0330

    feat: finish

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:responsive__chat/helpers/constants.dart';
+import 'package:responsive__chat/pages/responsive/desktop_web.dart';
+import 'package:responsive__chat/pages/responsive/mobile_tablet.dart';
+import 'package:responsive__chat/pages/responsive/responsive_layout_builder.dart';
+import 'package:responsive__chat/state/app_state_notifier.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppStateNotifier(
+        child: ResponsiveLayoutBuilder(
+            mobile: MobileTablet(selectedPage: SelectedPage.bodySelectedPage),
+            tablet: MobileTablet(
+                selectedPage: SelectedPage.bodySelectedPageSplitScreen),
+            desktopWeb: DesktopWeb(
+                selectedPage: SelectedPage.bodySelectedPageSplitScreen)));
   }
 }
--- lib/pages/responsive/desktop_web.dart
+++ lib/pages/responsive/desktop_web.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+
 
 import 'package:flutter/material.dart';
 import 'package:responsive__chat/helpers/constants.dart';
